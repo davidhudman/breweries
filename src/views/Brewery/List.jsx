5ede66ce7f496d50b0e81234bd160ee71b3cfb12
davidhudman <davidhudman@hotmail.com>
2022-06-30 16:28:28 -0400

    basic display of 10 breweries

--- src/views/Brewery/List.jsx
+++ src/views/Brewery/List.jsx
@@ -1,6 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function BreweryList() {
+  const [breweries, setBreweries] = useState(null);
+
+  const getBreweries = () => {
+    if (breweries === null) {
+      axios.get(`https://api.openbrewerydb.org/breweries`).then((response) => {
+        // set state as first 10 breweries from response
+        setBreweries(response.data.slice(0, 10));
+      });
+    }
+  };
+
+  getBreweries();
+
   return (
     <main>
       <h1>Brewery Catalog</h1>
@@ -10,9 +25,15 @@ export default function BreweryList() {
         <button type='reset'>Reset</button>
       </form>
       <ul>
-        <li>
-          <Link to='/breweries/1'>Brewery 1</Link> - Brewtown, OR
-        </li>
+        {breweries &&
+          breweries.map((brewery) => {
+            return (
+              <li key={brewery.id}>
+                <Link to={`/brewery/${brewery.id}`}>{brewery.name}</Link> -{" "}
+                {brewery.city}, {brewery.state}
+              </li>
+            );
+          })}
       </ul>
     </main>
   );
