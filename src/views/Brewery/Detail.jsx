0cc47e822a64e1fe498cf8f9b32a0933dcf0767f
davidhudman <davidhudman@hotmail.com>
2022-06-30 16:44:28 -0400

    show details of each brewery

--- src/views/Brewery/Detail.jsx
+++ src/views/Brewery/Detail.jsx
@@ -1,18 +1,45 @@
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function BreweryDetail() {
   const { id } = useParams();
+  const [brewery, setBrewery] = useState(null);
+
+  // search openbrewerydb.org for brewery with id
+  // and set state as response
+  const getBrewery = () => {
+    if (brewery === null) {
+      axios
+        .get(`https://api.openbrewerydb.org/breweries/${id}`)
+        .then((response) => {
+          setBrewery(response.data);
+        });
+    }
+  };
+
+  getBrewery();
 
   return (
     <main>
-      <h1>Brewery {id}</h1>
-      <p>Brewtown, Oregon 12345</p>
-      <p>United States</p>
-      <p>8005551234</p>
-      <p>
-        <a href='https://example.com'>View Website</a>
-      </p>
-      <Link to='/breweries'>Back to Breweries</Link>
+      {brewery && (
+        <>
+          <h1>{brewery.name}</h1>
+          <p>
+            <strong>
+              {brewery.city}, {brewery.state}
+            </strong>
+          </p>
+          <p>
+            <strong>{brewery.country}</strong>
+          </p>
+          <p>{brewery.phone}</p>
+          <p>
+            <a href={brewery.website_url}>Visit Website</a>
+          </p>
+        </>
+      )}
+      <Link to="/breweries">Back to Breweries</Link>
     </main>
   );
 }
