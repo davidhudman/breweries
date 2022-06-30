c0642149301f26f715e8b351838aa411a39b59af
davidhudman <davidhudman@hotmail.com>
2022-06-30 17:02:37 -0400

    bootstrap card formatting

--- src/views/Brewery/List.jsx
+++ src/views/Brewery/List.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import axios from "axios";
 // bootstrap
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 export default function BreweryList() {
   const [breweries, setBreweries] = useState(null);
@@ -50,28 +50,33 @@ export default function BreweryList() {
         </button>
         <button type="reset">Reset</button>
       </form>
+      <br />
+
+      <h2>Search Results:</h2>
       <>
         {breweries &&
           breweries.map((brewery) => {
             // return each brewery as a boostrap card
             return (
               <div key={brewery.id}>
-                <Container>
-                  <Row>
-                    <Col>
-                      <Link to={`/breweries/${brewery.id}`}>
-                        <strong>{brewery.name}</strong>
-                      </Link>
-                    </Col>
-                    <Col>
-                      <p>
-                        <strong>
-                          {brewery.city}, {brewery.state}
-                        </strong>
-                      </p>
-                    </Col>
-                  </Row>
-                </Container>
+                <Card style={{ width: "50rem" }}>
+                  <Container>
+                    <Row>
+                      <Col>
+                        <Link to={`/breweries/${brewery.id}`}>
+                          <strong>{brewery.name}</strong>
+                        </Link>
+                      </Col>
+                      <Col>
+                        <p>
+                          <strong>
+                            {brewery.city}, {brewery.state}
+                          </strong>
+                        </p>
+                      </Col>
+                    </Row>
+                  </Container>
+                </Card>
               </div>
             );
           })}
